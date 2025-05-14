84b6d22a23cae048f43797ee45f99110041d9ee0
Gonzalo251104 <102093183+Gonzalo251104@users.noreply.github.com>
2025-05-14 05:54:29 -0500

    Update Profile-page

--- src/app/profile/pages/profile-page/profile-page.component.ts
+++ src/app/profile/pages/profile-page/profile-page.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {NgIf, TitleCasePipe} from '@angular/common';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {MatCard, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [],
   templateUrl: './profile-page.component.html',
-  styleUrl: './profile-page.component.css'
+  imports: [
+    NgIf,
+    MatButtonToggle,
+    MatButtonToggleGroup,
+    TitleCasePipe,
+    MatCardTitle,
+    MatCard
+  ],
+  styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent {
+  userRole: 'diner' | 'chef' | null = null;
 
+  selectRole(role: 'diner' | 'chef') {
+    this.userRole = role;
+    localStorage.setItem('userRole', role);
+  }
+
+  canCreatePost(): boolean {
+    return this.userRole === 'chef';
+  }
 }

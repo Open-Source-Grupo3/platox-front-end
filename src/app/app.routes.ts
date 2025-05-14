07b38dba203a8e4ed25564a11a46be2a7d703ce1
Salvador Chamorro <salvadortres@hotmail.com>
2025-05-13 19:42:25 -0500

    fix: updated app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './public/pages/home/home.component';
+import {PostsPageComponent} from './posts/pages/posts-page/posts-page.component';
+import {ProfilePageComponent} from './profile/pages/profile-page/profile-page.component';
+import {PostInfoViewComponent} from './posts/pages/post-info-view/post-info-view.component';
 
-export const routes: Routes = [];
+
+const PageNotFoundComponent = () =>
+  import('./public/pages/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent);
+
+export const routes: Routes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'posts', component: PostsPageComponent },
+  { path: 'posts/post-info/:id', component: PostInfoViewComponent},
+  { path: 'profile', component: ProfilePageComponent },
+  { path: '',      redirectTo: '/home', pathMatch: 'full' },
+  { path: '**',    loadComponent: PageNotFoundComponent },
+
+];
